d707667096111163444cec44a0fd87d71148d304
chiencheng <chiencheng.yu@powertech.tw>
2020-02-04 18:00:11 +0800

    first commit

---
+++ BitriseDemo/BitriseDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitriseDemo
+//
+//  Created by Powertech on 2020/2/4.
+//  Copyright © 2020 TMT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
